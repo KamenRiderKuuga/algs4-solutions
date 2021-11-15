bf67ccf329488c9ec61a51c69eab09d9e1df82d0
GadHao <524472212@qq.com>
2021-11-15 21:13:22 +0800

    feat: add solution of exercise 1.1.2

---
+++ src/ch1_1/Ex02.java
@@ -0,0 +1,12 @@
+package ch1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        StdOut.println((1 + 2.236) / 2);
+        StdOut.println(1 + 2 + 3 + 4.0);
+        StdOut.println(4.1 >= 4);
+        StdOut.println(1 + 2 + "3");
+    }
+}
---
+++ src/ch1_1/Ex03.java
@@ -0,0 +1,11 @@
+package ch1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        if ((args).equals(null) || args.length < 3) {
+            StdOut.println("请从命令行输入三个参数");
+        }
+    }
+}
