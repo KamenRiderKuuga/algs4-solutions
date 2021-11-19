dbbef199f21df25fd9d37ed09150f72b2c57438e
GadHao <524472212@qq.com>
2021-11-19 18:14:35 +0800

    feat: solution of exercise 1.1.20

--- src/ch1_1/Ex20.java
+++ src/ch1_1/Ex20.java
@@ -1,37 +1,17 @@
 package ch1_1;
 
-import java.math.BigInteger;
-import java.util.Arrays;
-
 import edu.princeton.cs.algs4.StdOut;
 
 public class Ex20 {
-    public static long F(int N) {
-        if (N == 0)
-            return 0;
-        if (N == 1)
-            return 1;
-        return F(N - 1) + F(N - 2);
+    public static void main(String[] args) {
+        StdOut.println(lnFactorial(10));
     }
 
-    public static BigInteger iterationF(int N) {
-        BigInteger[] processArray = new BigInteger[N + 1];
-        for (int i = 0; i <= N; i++) {
-            if (i == 0 || i == 1) {
-                processArray[i] = BigInteger.valueOf(i);
-            } else {
-                processArray[i] = processArray[i - 1].add(processArray[i - 2]);
-            }
+    public static double lnFactorial(int n) {
+        if (n == 1) {
+            return 0;
         }
 
-        StdOut.println(Arrays.toString(processArray));
-        return processArray[100];
-    }
-
-    public static void main(String[] args) {
-        StdOut.println(iterationF(100));
-
-        for (int N = 0; N < 100; N++)
-            StdOut.println(N + " " + F(N));
+        return Math.log(n) + lnFactorial(n - 1);
     }
 }
