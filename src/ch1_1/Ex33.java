50622bd5c7bffea18563a252afd67ad0a724c263
GadHao <524472212@qq.com>
2021-11-30 23:29:53 +0800

    feat: part of the solution for exercise 1.33

--- src/ch1_1/Ex33.java
+++ src/ch1_1/Ex33.java
@@ -1,7 +1,41 @@
 package ch1_1;
 
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.StdOut;
+
 public class Ex33 {
     public static void main(String[] args) {
+        // test the dot
+        double[] firstArray = new double[] { 1, 2, 3, 4, 5 };
+        double[] secondArray = new double[] { 5, 4, 3, 2, 1 };
+        StdOut.println(Matrix.dot(firstArray, secondArray));
 
+        // test the mult
+        double[][] multA = new double[2][];
+        multA[0] = new double[] { 1, 2, 3 };
+        multA[1] = new double[] { 4, 5, 6 };
+
+        double[][] multB = new double[3][];
+        multB[0] = new double[] { 7, 8 };
+        multB[1] = new double[] { 9, 10 };
+        multB[2] = new double[] { 11, 12 };
+
+        double[][] multResult = Matrix.mult(multA, multB);
+        for (double[] ds : multResult) {
+            StdOut.println(Arrays.toString(ds));
+        }
+
+        // test the transpose
+        double[][] a = new double[5][];
+        a[0] = new double[] { 1, 4, 3 };
+        a[1] = new double[] { 8, 2, 6 };
+        a[2] = new double[] { 7, 8, 3 };
+        a[3] = new double[] { 4, 9, 6 };
+        a[4] = new double[] { 7, 8, 1 };
+        double[][] transposedMatrix = Matrix.transpose(a);
+        for (double[] ds : transposedMatrix) {
+            StdOut.println(Arrays.toString(ds));
+        }
     }
 }
--- src/ch1_1/Matrix.java
+++ src/ch1_1/Matrix.java
@@ -13,4 +13,29 @@ public class Matrix {
 
         return result;
     }
+
+    static double[][] mult(double[][] a, double[][] b) {
+        // the column count of first matrix must be same with the row count of second
+        double[][] result = new double[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                for (int k = 0; k < b[0].length; k++) {
+                    result[i][k] += a[i][j] * b[j][k];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static double[][] transpose(double[][] a) {
+        double[][] result = new double[a[0].length][a.length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                result[j][i] = a[i][j];
+            }
+        }
+
+        return result;
+    }
 }
